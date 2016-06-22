b54a293e90fdf01513c309dbd977ddfbcfbe3053
Norman Ettedgui <nettedgu@outlook.com>
2016-06-21 23:18:09 -0700

    Added blue d20 icon

--- app/src/main/java/com/example/leddkire/charactersheet5e/LoadCharListActivity.java
+++ app/src/main/java/com/example/leddkire/charactersheet5e/LoadCharListActivity.java
@@ -27,14 +27,6 @@ public class LoadCharListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         DatabaseHandler db = new DatabaseHandler(this);
@@ -43,5 +35,6 @@ public class LoadCharListActivity extends AppCompatActivity {
         ListAdapter listA = new ArrayAdapter<Character>(this, android.R.layout.simple_list_item_1, characters);
         listV.setAdapter(listA);
 
+        db.get_Character('1');
     }
 }
